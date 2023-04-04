854c51a35e4b1d62f802763aea0948f8c7947e55
nmbzdwss <1428750478@qq.com>
2023-04-05 02:11:30 +0800

    加密文件

---
+++ Assets/Scripts/弹窗/DialogController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Time.timeScale = 0;
+    }
+   
+    public void MouseClick()
+    {
+
+        GameObject g = GameObject.FindWithTag("Dialog");
+        Debug.Log("MouseClick");
+        Time.timeScale = 1;
+        Destroy(g);
+
+    }
+}
